5423694f8bf42e713f81aa51ba60514481cf8910
LeThiYenLuaB1910408 <luaB1910408@student.ctu.edu.vn>
2022-10-27 08:24:16 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <h1>Hiiiii</h1>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
