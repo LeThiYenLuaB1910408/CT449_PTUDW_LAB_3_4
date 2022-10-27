702ff969634e3c8017c532bc9379362ce08d6516
LeThiYenLuaB1910408 <luaB1910408@student.ctu.edu.vn>
2022-10-27 08:44:50 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const routes = [
         path: "/",
         name: "ContactBook",
         component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import('@/views/NotFound.vue'),
     }
 ];
 const router = createRouter(
